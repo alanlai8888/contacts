f6cc86dc304261b9d6c237ccc64dc4569fce920d
alanlai8888 <alai@blitzagency.com>
2016-06-20 14:36:53 -0700

    changed show/hide function from bootstrap tocss class

--- contactscript.js
+++ contactscript.js
@@ -1,4 +1,34 @@
-var a = document.getElementById("name");
-a.onclick = function(){
-	("#collapse1").slideDown();
+var names = document.getElementById("name");
+var show = document.getElementById("show");
+var hidden = document.getElementById("hide");
+
+//"accordion-body collapse" for emails
+function dropfunction(){
+	document.getElementById("mydropdown").classList.toggle("show");
 }
+function clickname2(){
+	if(hidden.style.display == "none")
+		hidden.style.display == "block";
+	else{
+		hidden.style.display = "none";
+	}
+}
+function toggle_visibility(id) {
+   var e = document.getElementById(id);
+   if(e.style.display == 'block')
+      e.style.display = 'none';
+   else
+      e.style.display = 'block';
+}
+$('#name').click(function(){
+	if(hidden.style.display == "none")
+		hidden.style.display == "block";
+	else{
+		hidden.style.display = "none";
+	}
+});
+$("#phone").click(function(){
+})
+
+$("#email").click(function(e){
+})
