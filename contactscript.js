c21d6686640d632b90154dbef184554a9663598c
alanlai8888 <alai@blitzagency.com>
2016-06-21 11:20:57 -0700

    delete unnecessary code, add the functionality for dropdown menu

--- contactscript.js
+++ contactscript.js
@@ -1,7 +1,8 @@
 var names = document.getElementById("name");
 var show = document.getElementById("show");
 var hidden = document.getElementById("hide");
-
+var info1s = document.getElementsByClassName('info1');
+var info2s = document.getElementsByClassName('info2');
 //"accordion-body collapse" for emails
 function dropfunction(){
 	document.getElementById("mydropdown").classList.toggle("show");
@@ -13,6 +14,24 @@ function clickname2(){
 		hidden.style.display = "none";
 	}
 }
+function showphone_hidemail(){
+	var i, j;
+	for (i=0; i<info1s.length; i++){
+		info1s[i].style.display = 'block';
+	}
+	for (j=0; j<info2s.length; j++){
+		info2s[j].style.display = 'none'; 
+	}
+}
+function showemail_hidephone(){
+	var i, j;
+	for (i=0; i<info1s.length; i++){
+		info2s[i].style.display = 'block';
+	}
+	for (j=0; j<info2s.length; j++){
+		info1s[j].style.display = 'none'; 
+	}
+}
 function toggle_visibility(id) {
    var e = document.getElementById(id);
    if(e.style.display == 'block')
@@ -20,15 +39,17 @@ function toggle_visibility(id) {
    else
       e.style.display = 'block';
 }
-$('#name').click(function(){
-	if(hidden.style.display == "none")
-		hidden.style.display == "block";
-	else{
-		hidden.style.display = "none";
-	}
-});
-$("#phone").click(function(){
-})
 
-$("#email").click(function(e){
-})
+function information(type) {
+	switch(type){
+		case "phone":
+			console.log("clicked phone");
+			showphone_hidemail();
+			break;
+		case "email":
+			console.log("clicked email");
+			showemail_hidephone();
+			break;
+	}
+
+}
